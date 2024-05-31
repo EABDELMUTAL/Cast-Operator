6be0d7c36991a407a2f966016dfe208b5ee943d4
ELEANOR ABDELMUTAL <EABDELMUTAL87608@uat.edu>
2024-05-31 08:29:20 -0400

    Add project files.

---
+++ Cast Operator/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Cast_Operator
+{
+    partial class castOperator1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.realNumberLabel = new System.Windows.Forms.Label();
+            this.realNumberTextBox = new System.Windows.Forms.TextBox();
+            this.wholeNumberLabel = new System.Windows.Forms.Label();
+            this.outputLabelWholeNumber = new System.Windows.Forms.Label();
+            this.caluclateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.wholeNumberLable1 = new System.Windows.Forms.Label();
+            this.wholeNumberTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // realNumberLabel
+            // 
+            this.realNumberLabel.BackColor = System.Drawing.Color.White;
+            this.realNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.realNumberLabel.Location = new System.Drawing.Point(49, 70);
+            this.realNumberLabel.Name = "realNumberLabel";
+            this.realNumberLabel.Size = new System.Drawing.Size(146, 23);
+            this.realNumberLabel.TabIndex = 0;
+            this.realNumberLabel.Text = "Real Number";
+            this.realNumberLabel.Click += new System.EventHandler(this.wholeNumber1_Click);
+            // 
+            // realNumberTextBox
+            // 
+            this.realNumberTextBox.Location = new System.Drawing.Point(227, 71);
+            this.realNumberTextBox.Name = "realNumberTextBox";
+            this.realNumberTextBox.Size = new System.Drawing.Size(486, 20);
+            this.realNumberTextBox.TabIndex = 1;
+            //this.realNumberTextBox.TextChanged += new System.EventHandler(this.realNumberTextBox_TextChanged_1);
+            // 
+            // wholeNumberLabel
+            // 
+            this.wholeNumberLabel.BackColor = System.Drawing.Color.White;
+            this.wholeNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wholeNumberLabel.Location = new System.Drawing.Point(46, 154);
+            this.wholeNumberLabel.Name = "wholeNumberLabel";
+            this.wholeNumberLabel.Size = new System.Drawing.Size(149, 23);
+            this.wholeNumberLabel.TabIndex = 2;
+            this.wholeNumberLabel.Text = "Whole Number\r\n";
+            // 
+            // outputLabelWholeNumber
+            // 
+            this.outputLabelWholeNumber.BackColor = System.Drawing.Color.White;
+            this.outputLabelWholeNumber.Location = new System.Drawing.Point(227, 157);
+            this.outputLabelWholeNumber.Name = "outputLabelWholeNumber";
+            this.outputLabelWholeNumber.Size = new System.Drawing.Size(486, 23);
+            this.outputLabelWholeNumber.TabIndex = 3;
+            this.outputLabelWholeNumber.Click += new System.EventHandler(this.outputLabelWholeNUmber_Click);
+            // 
+            // caluclateButton
+            // 
+            this.caluclateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.caluclateButton.Location = new System.Drawing.Point(453, 214);
+            this.caluclateButton.Name = "caluclateButton";
+            this.caluclateButton.Size = new System.Drawing.Size(75, 23);
+            this.caluclateButton.TabIndex = 4;
+            this.caluclateButton.Text = "Calculate";
+            this.caluclateButton.UseVisualStyleBackColor = true;
+            this.caluclateButton.Click += new System.EventHandler(this.caluclateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(540, 213);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(629, 212);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // wholeNumberLable1
+            // 
+            this.wholeNumberLable1.BackColor = System.Drawing.Color.White;
+            this.wholeNumberLable1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wholeNumberLable1.Location = new System.Drawing.Point(47, 115);
+            this.wholeNumberLable1.Name = "wholeNumberLable1";
+            this.wholeNumberLable1.Size = new System.Drawing.Size(149, 23);
+            this.wholeNumberLable1.TabIndex = 7;
+            this.wholeNumberLable1.Text = "Whole Number\r\n";
+            // 
+            // wholeNumberTextBox
+            // 
+            this.wholeNumberTextBox.Location = new System.Drawing.Point(225, 116);
+            this.wholeNumberTextBox.Name = "wholeNumberTextBox";
+            this.wholeNumberTextBox.Size = new System.Drawing.Size(486, 20);
+            this.wholeNumberTextBox.TabIndex = 8;
+            this.wholeNumberTextBox.TextChanged += new System.EventHandler(this.wholeNumberTextBox_TextChanged);
+            // 
+            // castOperator1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(754, 286);
+            this.Controls.Add(this.wholeNumberTextBox);
+            this.Controls.Add(this.wholeNumberLable1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.caluclateButton);
+            this.Controls.Add(this.outputLabelWholeNumber);
+            this.Controls.Add(this.wholeNumberLabel);
+            this.Controls.Add(this.realNumberTextBox);
+            this.Controls.Add(this.realNumberLabel);
+            this.Name = "castOperator1";
+            this.Text = "Cast Operator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label realNumberLabel;
+        private System.Windows.Forms.TextBox realNumberTextBox;
+        private System.Windows.Forms.Label wholeNumberLabel;
+        private System.Windows.Forms.Label outputLabelWholeNumber;
+        private System.Windows.Forms.Button caluclateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label wholeNumberLable1;
+        private System.Windows.Forms.TextBox wholeNumberTextBox;
+    }
+}
+
---
+++ Cast Operator/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cast_Operator
+{
+    public partial class castOperator1 : Form
+    {
+        public castOperator1()
+        {
+            InitializeComponent();
+        }
+
+        private void outputLabelWholeNUmber_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wholeNumber1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void realNumberTextBox_TextChanged(object sender, EventArgs e)
+        {
+            int wholeNumber;
+            double realNumber = 3.0;
+            wholeNumber = (int)realNumber;
+        }
+
+        private void wholeNumberTextBox_TextChanged(object sender, EventArgs e)
+        {
+            int wholeNumber;
+            double realNumber = 3.0;
+            wholeNumber = (int)realNumber;
+        }
+
+        private void caluclateButton_Click(object sender, EventArgs e)
+        {
+            int wholeNumber;
+            double realNumber = 3.0;
+            realNumber = double.Parse(realNumberTextBox.Text);
+            wholeNumber = (int)realNumber;
+            outputLabelWholeNumber.Text = wholeNumber.ToString();
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            realNumberTextBox.Text = "";
+        }
+
+    }
+}
